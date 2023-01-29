7d69334aaace81e6d38723d16fb61a8ca64f353d
nathanq <1532722889@qq.com>
2023-01-29 11:28:11 +0800

    update goodslist

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,28 @@
 <template>
 	<view>
-		
+		<view class="goods-list">
+			<block v-for="(goods, i) in goodsList" :key="i">
+				<view class="goods-item">
+					<!-- 商品左侧图片区域 -->
+					<view class="goods-item-left">
+						<image :src="goods.goods_small_logo || defaultPic" class="goods-pic"></image>
+					</view>
+					<!-- 商品右侧信息区域 -->
+					<view class="goods-item-right">
+						<!-- 商品标题 -->
+						<view class="goods-name">{{goods.goods_name}}</view>
+						<view class="goods-info-box">
+							<!-- 商品价格 -->
+							<view class="goods-price">￥{{goods.goods_price | tofixed}}</view>
+						</view>
+					</view>
+				</view>
+			</block>
+			<block v-for="(item, i) in goodsList" :key="i" @click="gotoDetail(item)">
+				<!-- 为 my-goods 组件动态绑定 goods 属性的值 -->
+				<my-goods :goods="item"></my-goods>
+			</block>
+		</view>
 	</view>
 </template>
 
@@ -8,12 +30,98 @@
 	export default {
 		data() {
 			return {
-				
-			};
+				defaultPic: 'https://img3.doubanio.com/f/movie/8dd0c794499fe925ae2ae89ee30cd225750457b4/pics/movie/celebrity-default-medium.png',
+				//请求参数对象
+				queryObj: {
+					//查询关键词
+					query: '',
+					//商品分类id
+					cid: '',
+					//页码值
+					pagenum: 1,
+					//每页显示多少条数据
+					pagesize: 10
+				},
+				goodsList: [],
+				total: 0,
+				isloading: false
+			}
+		},
+		filters: {
+			// 把数字处理为带两位小数点的数字
+			tofixed(num) {
+				return Number(num).toFixed(2)
+			}
+		},
+		methods: {
+			async getGoodsList(cb) {
+				this.isloading = true
+				// 发起请求
+				const { data: res } = await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+				this.isloading = false
+				cb && cb()
+				if (res.meta.status !== 200) return uni.$showMsg()
+				// 为数据赋值
+				this.goodsList = [...this.goodsList, ...res.message.goods]
+				this.total = res.message.total
+			},
+			gotoDetail(item) {
+				uni.navigateTo({
+					url: '/subpkg/goods_detail/goods_detail?goods_id=' + item.goods_id
+				})
+			},
+			onLoad(options) {
+				this.queryObj.query = options.query || ''
+				this.queryObj.cid = options.cid || ''
+				this.getGoodsList()
+			},
+			onReachBottom() {
+				if (this.queryObj.pagenum * this.queryObj.pagesize >= this.total) return uni.$showMsg('数据加载完毕！')
+				if (this.isloading) return
+				this.queryObj.pagenum += 1
+				this.getGoodsList()
+			},
+			onPullDownRefresh() {
+				this.queryObj.pagenum = 1
+				this.total = 0
+				this.isloading = false
+				this.goodsList = []
+				this.getGoodsList(() => uni.stopPullDownRefresh())
+			}
 		}
+
 	}
 </script>
 
 <style lang="scss">
+	.goods-item {
+		display: flex;
+		padding: 10px 5px;
+		border-bottom: 1px solid #f0f0f0;
 
+		.goods-item-left {
+			margin-right: 5px;
+
+			.goods-pic {
+				width: 100px;
+				height: 100px;
+				display: block;
+			}
+		}
+
+		.goods-item-right {
+			display: flex;
+			flex-direction: column;
+			justify-content: space-between;
+
+			.goods-name {
+				font-size: 13px;
+			}
+
+			.goods-price {
+				font-size: 16px;
+				color: #c00000;
+			}
+		}
+	}
 </style>
